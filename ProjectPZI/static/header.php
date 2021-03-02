0af60291c890bed13c66761c4fcd61aa27897a05
merimavele <73862655+merimavele@users.noreply.github.com>
2021-03-02 21:54:25 +0100

    Add files via upload

---
+++ ProjectPZI/static/header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/57c3439383.js" crossorigin="anonymous"></script>
+    <title><?php echo($naslov); ?></title>
+    <link href="logo.png" rel="icon" type="image/x-icon">
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <!-- bootbox.js at 4.4.0 -->
+    <script src="https://rawgit.com/makeusabrew/bootbox/f3a04a57877cab071738de558581fbc91812dce9/bootbox.js"></script>  
+    <style>
+        html, body {
+            height:100% !important;
+        }
+    </style>
+ 
+  </head>
+  <body>
