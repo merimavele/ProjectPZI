f7d0664db6bfdb51ecf2872fe96b3a9cff12bc3e
merimavele <73862655+merimavele@users.noreply.github.com>
2021-03-03 23:45:14 +0100

    Update db.php

--- ProjectPZI/db.php
+++ ProjectPZI/db.php
@@ -1,14 +1,10 @@
 <?php
-define("RACUNALO", "localhost");
-define("KORISNIK", "root");
-define("LOZINKA", "");
-define("BAZA", "onlinemath");
+    $konekcija=mysqli_connect('localhost','fpmoz062021','csdigital2021','fpmoz062021');
 
-$konekcija = mysqli_connect(RACUNALO, KORISNIK, LOZINKA, BAZA);
 
 if (!$konekcija) {
     die("Nismo se spojili na bazu iz nekog razloga, možda znate o čemu se radi, evo greška: " . mysqli_connect_error());
 }
 ?>
-<?php
+
 
