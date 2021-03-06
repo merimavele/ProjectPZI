ff1c7293b68035dd87f37458bb8db6a0819d7a3b
merimavele <73862655+merimavele@users.noreply.github.com>
2021-03-06 23:20:17 +0100

    Update index.php

--- ProjectPZI/index.php
+++ ProjectPZI/index.php
@@ -1,24 +1,5 @@
-<!DOCTYPE html>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="css/style.css">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    <title>OnlineMaths</title>
-    <link href="logo.png" rel="icon" type="image/x-icon">
-</head>
-<body style="background-image: url(hated_math_1200x627.jpg);">
-<?php include("static/header.php"); ?>
-
-</header>
+<?php $naslov= "Prijava";
+ include("static/header.php"); ?>
 <div class=".container-fluid">
 <div class= "row" style="margin-top: 15px; padding-left: 30px;">
 <div class="col-sm-7">
