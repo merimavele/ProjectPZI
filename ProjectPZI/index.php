8c4431a2aea6d1142e8a8abc1a9af91fd5340282
merimavele <73862655+merimavele@users.noreply.github.com>
2021-03-07 14:57:09 +0100

    Update index.php

--- ProjectPZI/index.php
+++ ProjectPZI/index.php
@@ -1,5 +1,7 @@
 <?php $naslov= "Prijava";
  include("static/header.php"); ?>
+
+
 <div class=".container-fluid">
 <div class= "row" style="margin-top: 15px; padding-left: 30px;">
 <div class="col-sm-7">
@@ -9,11 +11,14 @@
     padding-right: 50px;">
      <div class="span6" style="
     border-top-width: 30px;
-    padding-top: 50px;">
+    padding-top: 20px;">
     <div class="pull-right"><?php include('login.php'); ?></div>
 		</div>
         </div>
         </div>
         </div>
+        <?php include('podnozje.php'); ?>
+
+
        
 
