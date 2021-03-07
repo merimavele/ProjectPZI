804585e15a06681fe102b1e65d65de3bc3be0d05
merimavele <73862655+merimavele@users.noreply.github.com>
2021-03-07 01:01:27 +0100

    Update index.php

--- ProjectPZI/index.php
+++ ProjectPZI/index.php
@@ -3,13 +3,13 @@
 <div class=".container-fluid">
 <div class= "row" style="margin-top: 15px; padding-left: 30px;">
 <div class="col-sm-7">
-<img src="online-teaching-challenges.jpg" width="700"  height="400" align="left" vspace="100"  alt style=" margin-top: 15px;">
+<img src="online-teaching-challenges.jpg" width="780px"  height="460px"   alt style=" margin-top: 15px;">
      </div>
      <div class="col-sm-5" style="
     padding-right: 50px;">
      <div class="span6" style="
     border-top-width: 30px;
-    padding-top: 20px;">
+    padding-top: 50px;">
     <div class="pull-right"><?php include('login.php'); ?></div>
 		</div>
         </div>
